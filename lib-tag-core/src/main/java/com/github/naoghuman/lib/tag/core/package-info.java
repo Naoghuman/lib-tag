1ea1f01638c341a9e850b931b5d15f1dccf580bb
Naoghuman <peter.rogge@yahoo.de>
2017-07-15 11:21:01 +0200

    Add JavaDoc to the package-info.

--- lib-tag-core/src/main/java/com/github/naoghuman/lib/tag/core/package-info.java
+++ lib-tag-core/src/main/java/com/github/naoghuman/lib/tag/core/package-info.java
@@ -15,9 +15,33 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /**
- * Contains the <code>API</code> for the library <code>Lib-Tag</code>.
+ * Contains the {@code API} for the library {@code Lib-Tag-Core}.
+ * <p>
+ * The main entry point from the API is the {@code Interface} {@link com.github.naoghuman.lib.tag.core.Tag} 
+ * which defines a Tag. A Tag is basically a simple {@link java.lang.String} which can be used for example 
+ * in a {@link javafx.scene.control.Button}, {@link javafx.scene.control.Label} or another {@code JavaFX} 
+ * gui component. Suchlike tagged topics can be easily searched or analysed for a Tag.<br>
+ * A Tag can be easily created and configured with the fluent builder {@code Class} 
+ * {@link com.github.naoghuman.lib.tag.core.TagBuilder} which is the preferred way to create an instance 
+ * from the {@code Interface} Tag. Another way would be to use directly the {@code Class} 
+ * {@link com.github.naoghuman.lib.tag.internal.DefaultTag} which isn't the recommended way.
+ * <p>
+ * A Tag can be easily mapped with an {@code JavaFX} gui component with the {@code Interface}
+ * {@link com.github.naoghuman.lib.tag.core.TagRelation}. Again a fluent builder {@code Class}
+ * {@link com.github.naoghuman.lib.tag.core.TagRelationBuilder} exists, which allowed to create
+ * and configured an instance from the {@code Interface} TagRelation.<br>
+ * Like the {@code Class} DefaultTag the {@code Class} 
+ * {@link com.github.naoghuman.lib.tag.internal.DefaultTagRelation} allowed to create and configured 
+ * directly an implementation from the {@code Interface} TagRelation, but again the preferred way is the 
+ * corresponding fluent builder.
  * 
  * @author Naoghuman
  * @since  0.1.0
+ * @see    com.github.naoghuman.lib.tag.core.Tag
+ * @see    com.github.naoghuman.lib.tag.core.TagBuilder
+ * @see    com.github.naoghuman.lib.tag.core.TagRelation
+ * @see    com.github.naoghuman.lib.tag.core.TagRelationBuilder
+ * @see    com.github.naoghuman.lib.tag.internal.DefaultTag
+ * @see    com.github.naoghuman.lib.tag.internal.DefaultTagRelation
  */
 package com.github.naoghuman.lib.tag.core;
