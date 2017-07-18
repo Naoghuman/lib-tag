443df3e757f65f77318cdf674f7a5826f53f2771
Naoghuman <peter.rogge@yahoo.de>
2017-07-18 20:01:11 +0200

    First version from the Classes and Interfaces in this library.

---
+++ lib-tag-components/src/main/java/com/github/naoghuman/lib/tag/components/internal/TagButton.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (C) 2017 Naoghuman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.naoghuman.lib.tag.components.internal;
+
+import com.github.naoghuman.lib.tag.core.Tag;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+/**
+ * TODO
+ * 
+ * @author Naoghuman
+ * @since  0.1.0
+ */
+public interface TagButton {
+
+    /**
+     * TODO
+     * 
+     * @param tagId
+     * @return
+     */
+    public EventHandler<ActionEvent> getActionHandlerForButton(final long tagId);
+
+    /**
+     * TODO
+     * 
+     * @param actionId
+     * @param tagId
+     * @return
+     */
+    public EventHandler<ActionEvent> getActionHandlerForButton(final String actionId, final long tagId);
+
+    /**
+     * TODO
+     * 
+     * @param tag
+     * @return
+     */
+    public Button getTagAsButton(final Tag tag);
+
+    /**
+     * TODO
+     * 
+     * @param tag
+     * @param eventHandler
+     * @return
+     */
+    public Button getTagAsButton(final Tag tag, final EventHandler<ActionEvent> eventHandler);
+
+    /**
+     * TODO
+     * 
+     * @param tag
+     * @param style
+     * @param eventHandler
+     * @return
+     */
+    public Button getTagAsButton(final Tag tag, final String style, final EventHandler<ActionEvent> eventHandler);
+
+    /**
+     * TODO
+     * 
+     * @param defaultActionId
+     */
+    public void setButtonDefaultActionId(final String defaultActionId);
+
+    /**
+     * TODO
+     * 
+     * @param defaultStyle
+     */
+    public void setButtonDefaultStyle(final String defaultStyle);
+
+}
